5b9b5802f0939465c5ef1dc045fdc2e73ac25baa
Thayara Siqueira <thay.siqu@gmail.com>
2022-09-16 15:51:09 -0300

    refactor: changing products validation to the service layer to keep the MSC architeture

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,15 +1,11 @@
 import { Request, Response } from 'express';
-import { isValidProduct } from '../middlewares/validations';
 import productsService from '../services/product.service';
 
 const productsController = {
   create: async (req:Request, res:Response) => {
-    const { code, message } = isValidProduct(req.body);
-    if (message) {
-      return res.status(code).json({ message });
-    }
-    const product = await productsService.create(req.body);
-    return res.status(201).json(product);
+    const { code, message } = await productsService.create(req.body);
+    if (typeof message === 'string') return res.status(code).json({ message });
+    return res.status(code).json(message);
   },
   getAll: async (req:Request, res:Response): Promise<Response> => {
     const products = await productsService.getAll();
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,12 +1,18 @@
 import { IProduct, Product } from '../interfaces/products.interfaces';
+import { isValidProduct } from '../middlewares/validations';
 import productsModel from '../models/products.model';
 
 const productsService = {
   create: async (product: IProduct) => {
+    const resultError = isValidProduct(product);
+    if (resultError.message) {
+      return resultError;
+    }
     const newProduct = product;
     const { insertId } = await productsModel.create(product);
     newProduct.id = insertId;
-    return newProduct;
+    const result = { code: 201, message: newProduct };
+    return result;
   },
   getAll: async ():Promise<Product[]> => {
     const products = await productsModel.getAll();
